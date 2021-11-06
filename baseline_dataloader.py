e0294c3304cfbaba799269765851331c71e5389c
tchainzzz <tchang97@stanford.edu>
2021-11-06 14:12:53 -0400

    add correlated MNIST dataset with customizable transformation

--- baseline_dataloader.py
+++ baseline_dataloader.py
@@ -1,60 +1,74 @@
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision import transforms
-import matplotlib.pyplot as plt
-
-transform_dict = {
-        'train': transforms.Compose(
-            [transforms.RandomCrop(25),
-             transforms.RandomRotation(degrees=180),
-             transforms.ToTensor(),
-			 #transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  #std=[0.229, 0.224, 0.225]), for ResNet
-			]),
-        'test': transforms.Compose(
-            [transforms.ToTensor(),
-             #transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  #std=[0.229, 0.224, 0.225]), for ResNet
-             ])}
+from torchvision import transforms as T
 
 
-def baseline_dataloader(mode="train", transform=transform_dict["train"], digits=[2,8]):
-	'''
-	Return MINIST dataloader.
+class CorrelatedMNIST(Dataset):
+    def __init__(
+        self,
+        mode="train",
+        spurious_feature_fn=T.RandomRotation(degrees=90),
+        spurious_corr_coeff=0.7,
+        digits=[2, 8],
+        normalize=False,
+        seed=42,
+    ):
+        torch.manual_seed(seed)
 
-	mode: "train" or "test"
-	transform: transform_dict["train"] or transform_dict["test"]
-	digits: 2 digits used as baseline
-	
-	'''
-	if mode == "train":
-		data = datasets.MNIST(
-			root="data",
-			train=True,
-			download=True,
-			transform=transform_dict["train"]
-		)
-		idx1 = (data.train_labels==digits[0])
-		idx2 = (data.train_labels==digits[1])
-		idx = torch.logical_or(idx1, idx2)
-		data.targets = data.targets[idx]
-		data.data = data.data[idx]
-	elif mode == "test":
-		data = datasets.MNIST(
-			root="data",
-			train=False,
-			download=True,
-			transform=transform_dict["test"]
-		)
-		idx1 = (data.test_labels==digits[0])
-		idx2 = (data.test_labels==digits[1])
-		idx = torch.logical_or(idx1, idx2)
-		data.targets = data.targets[idx]
-		data.data = data.data[idx]
-	else:
-		print("Invalid mode.")
-	return data
+        self.mode = mode
+        self.normalize = normalize
+
+        self.spurious_feature_fn = spurious_feature_fn
+        self.spurious_corr_coeff = spurious_corr_coeff
+        self.digits = digits
+        if mode == "train":
+            data = datasets.MNIST(
+                root="data",
+                train=True,
+                download=True,
+            )
+        elif mode == "test":
+            data = datasets.MNIST(
+                root="data",
+                train=False,
+                download=True,
+            )
+        else:
+            raise ValueError("Invalid mode.")
+        idx1 = (data.targets == digits[0])
+        idx2 = (data.targets == digits[1])
+        self.labels = data.targets[idx1 | idx2]
+        self.labels[self.labels == digits[0]] = 0
+        self.labels[self.labels == digits[1]] = 1
+        self.images = data.data[idx1 | idx2]
+
+    def __getitem__(self, idx):
+        X = self.images[idx].unsqueeze(0)
+        y = self.labels[idx]
+        """
+            Randomly draw domain:
+
+            z ~ Ber(spurious_corr_coeff * y + (1 - spurious_corr_coeff) * (1-y))
+        """
+        domain_threshold = self.spurious_corr_coeff if y else 1 - self.spurious_corr_coeff
+        z = (torch.rand(1) < domain_threshold).int()
+        if z:
+            X = self.spurious_feature_fn(X)
+        if self.normalize:
+            X = T.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                )(X)
+        return X, y, z 
+
+    def __len__(self):
+        return len(self.labels)
+
+
+
+
+
 '''
 # Sample code for visulization:
 
