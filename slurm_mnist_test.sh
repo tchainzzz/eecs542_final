03dabab0b40c3837154ddb13cd8aff9b01d50f06
Dinesh Samuel Sathia Raj <dineshsathiaraj2@gmail.com>
2021-11-24 20:04:25 -0500

    Updated bash script and training pipeline

--- slurm_mnist_test.sh
+++ slurm_mnist_test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=test.job
-#SBATCH --output=.slurm/test.out
-#SBATCH --error=.slurm/test.err
-#SBATCH --time=00:05:00
+#SBATCH --output=test.out
+#SBATCH --time=00:25:00
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=v100:1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=4g
-WANDB_MODE=offline python training_pipeline.py --n_epochs 1 --model_name resnet --opt_name Adam --lr 1e-3 --dataset mnist --wandb_expt_name ctrenton-gl-test --limit_batches 8 # sync afterwards
+#SBATCH --mem-per-cpu=4G
+
+echo LEGGOO
+module list
+nvidia-smi
+
+python training_pipeline.py --n_epochs 100 --model_name resnet --opt_name Adam --lr 1e-3 --dataset mnist --wandb_expt_name GL_test --batch_size 511 --num_workers 16 # sync afterwards
 
