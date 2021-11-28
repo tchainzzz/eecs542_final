2b0b802c05aa84c557f7cf08c8f572b4bedfe236
Patrick Sherry <ptrck@umich.edu>
2021-11-28 16:27:22 -0500

    Update job scripts with proper parameters

--- slurm/slurm_densnet_camelyon.sh
+++ slurm/slurm_densnet_camelyon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test.job
 #SBATCH --output=test.out
-#SBATCH --time=00:25:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=v100:1
@@ -11,4 +11,4 @@ echo LEGGOO
 module list
 nvidia-smi
 source ~/eecs542_final/env542/bin/activate
-python ~/eecs542_final/training_pipeline.py --n_epochs 10 --model_name densenet --opt_name Adam --lr 1e-3 --dataset camelyon17 --wandb_expt_name patr-test --batch_size 511 --num_workers 16 # sync afterwards
+python ~/eecs542_final/training_pipeline.py --n_epochs 5 --model_name densenet --opt_name SGD --opt_kwargs momentum=0.9 weight_decay=0.01 --lr 1e-3 --dataset camelyon17 --batch_size 32 --num_workers 16 --corr 0.9 --wandb_expt_name densenetCamelyon # sync afterwards
--- slurm/slurm_densnet_mnist.sh
+++ slurm/slurm_densnet_mnist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test.job
 #SBATCH --output=test.out
-#SBATCH --time=00:25:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=v100:1
@@ -11,4 +11,4 @@ echo LEGGOO
 module list
 nvidia-smi
 source ~/eecs542_final/env542/bin/activate
-python ~/eecs542_final/training_pipeline.py --n_epochs 10 --model_name densenet --opt_name Adam --lr 1e-3 --dataset mnist --wandb_expt_name patr-test --batch_size 511 --num_workers 16 # sync afterwards
+python ~/eecs542_final/training_pipeline.py --n_epochs 100 --model_name densenet --opt_name Adam --lr 1e-3 --dataset mnist --wandb_expt_name densenetMNIST --batch_size 511 --num_workers 16 --corr 0.9
